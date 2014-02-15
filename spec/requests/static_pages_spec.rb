44f5d654a07f9de4033270f296c07525bc13b349
Patrik McKiernan <patrik@mckiernan.com>
2014-02-15 18:36:07 +0100

    Shortened the custom css file by inheritence. Updated routes. Added helper for tests. Made tests smaller.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,57 +3,33 @@ require 'spec_helper'
 describe "StaticPages" do
     let(:stat) { "Ruby on Rails Tutorial Sample App" }
 
+    subject { page }
     #Test the Home Page
     describe "Home page" do
-        it "should have the h1 'Sample App'" do
-            visit '/static_pages/home'
-            page.should have_selector('h1', :text => 'Sample App')
-        end
-
-        it "should have the base title 'Home'" do
-            visit '/static_pages/home'
-            expect(page).to have_title("#{stat}")
-        end
-
-        it "should not have a custom page title" do
-            visit '/static_pages/home'
-            expect(page).not_to have_title('| Home')
-        end
+        before { visit root_path }
+        it { should have_content('Sample App') }
+        it { should have_title( full_title('') ) }
+        it { should_not have_title('| Home') }
     end
 
   #Test the Help Page
     describe "Help page" do
-        it "should have the h1 'Help'" do
-            visit '/static_pages/help'
-            page.should have_selector('h1', :text => 'Help')
-        end
-        it "should have the right title" do
-            visit '/static_pages/help'
-            expect(page).to have_title("#{stat} | Help")
-        end
+        before { visit help_path }
+        it { should have_content('Help') }
+        it { should have_title(full_title('Help')) }
     end
     
     #Test the About Page
     describe "About page" do
-        it "should have the content 'About Us'" do
-            visit '/static_pages/about'
-            expect(page).to have_content('About Us')
-        end
-        it "should have the right title" do
-            visit '/static_pages/about'
-            expect(page).to have_title("#{stat} | About Us")
-        end
+        before { visit about_path }
+        it { should have_content('About') }
+        it { should have_title(full_title('About Us')) }
     end
 
     #Test the Contact Page 
     describe "Contact page" do
-        it "should have the content 'Contact'" do
-            visit '/static_pages/contact'
-            expect(page).to have_content('Contact')
-        end
-        it "should have the right title" do
-            visit '/static_pages/contact'
-            expect(page).to have_title("#{stat} | Contact")
-        end
+        before { visit contact_path }
+        it { should have_content('Contact') }
+        it { should have_title(full_title('Contact')) }
     end
 end
