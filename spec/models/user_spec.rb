e8ec3965b0913b337e846bb39a6c6c03ab477ffe
Patrik McKiernan <patrik@mckiernan.com>
2014-02-22 13:48:15 +0100

    Added more detailed regex that coveres invalid email addresses with two consecutive dots after the @, like so patrik@email..com. Added test to make sure emails of mixed case is saved to the database in lower case.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pp'
 
 describe User do
     before do
@@ -35,7 +36,7 @@ describe User do
     describe "when email format is invalid" do
         it "should be invalid" do
             addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                                 foo@bar_baz.com foo@bar+baz.com]
+                                 foo@bar_baz.com foo@bar+baz.com foo@bar..com]
             addresses.each do |invalid_address|
                 @user.email = invalid_address
                 expect(@user).not_to be_valid
@@ -61,6 +62,22 @@ describe User do
         end
 
         it { should_not be_valid }
+    end
+
+    describe "email address with mixed case" do
+        let(:mixed_case_email) { "FoO@ExaMPLe.coM" }
+        it "should be saved with lower case" do
+            @user.email = mixed_case_email
+            @user.save
+            @user.reload
+            #pp "mixed: " + mixed_case_email
+            #pp "reload: " + @user.email
+            #Rails.logger.debug @user.email.inspect
+            #Rails.logger.debug @user.email.to_yaml
+            #puts @user.email.to_yaml
+            #expect(mixed_case_email).to_not eq @user.email   
+            expect(@user.reload.email).to eq mixed_case_email.downcase
+        end
     end
 
     describe "when password is not present" do
