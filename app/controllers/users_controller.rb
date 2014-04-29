39307b8f62f6bd357ac3a0803f5b49fb54736ee9
Patrik McKiernan <patrik@mckiernan.com>
2014-04-29 19:56:37 +0200

    Allow updating users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,28 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+
+    def edit
+        # Rails.logger = Logger.new(STDOUT)
+        # Rails.logger.debug("----Params: " + params.inspect)
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        Rails.logger = Logger.new(STDOUT)
+        # Rails.logger.debug("----Params: " + params.inspect)
+        # Rails.logger.debug("----User: " + @user.inspect)
+
+        if @user.update_attributes(user_params)
+            Rails.logger.info(" %%%%%%%%%%%%%%%%User update successfully")
+            flash[:success] = "Profile updated"
+            redirect_to @user
+        else
+            Rails.logger.info(@user.errors.messages.inspect)
+            render 'edit'
+        end
+    end
  
     private
  
